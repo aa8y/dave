5763afc33fc82329aa6e66be7bdac4553b89d5dd
Arun Allamsetty <arun.allamsetty@gmail.com>
2024-06-23 17:05:38 -0700

    Update Node and dependencies
    - Update to versions which do not break current functionality.

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.mocha,
+            ...globals.node,
+        },
+
+        ecmaVersion: 6,
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        "no-console": [0],
+        semi: ["error", "never"],
+        "space-before-function-paren": ["error", "never"],
+        "spaced-comment": ["error", "always"],
+    },
+}];
